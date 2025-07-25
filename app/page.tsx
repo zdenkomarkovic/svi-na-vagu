a1f706d40a23f22f91a4ae9250519a298173cdd1
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-07-25 17:01:37 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
       <PozoviteNasOdmah />
       <Statistika />
       <Cardlist />
-      <BackgroundImage />
       <CardWithImage />
+      <BackgroundImage />
       <VideoCard />
     </div>
   );
